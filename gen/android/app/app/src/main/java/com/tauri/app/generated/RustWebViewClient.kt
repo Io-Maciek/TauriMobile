58256e08c1bb9d01eba1cb8d0d8845389f2df7ae
Maciej <mkuchcik@gmail.com>
2023-02-26 10:02:38 +0100

    generated

---
+++ gen/android/app/app/src/main/java/com/tauri/app/generated/RustWebViewClient.kt
@@ -0,0 +1,33 @@
+/* THIS FILE IS AUTO-GENERATED. DO NOT MODIFY!! */
+
+// Copyright 2020-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
+
+package com.tauri.app
+
+import android.webkit.*
+
+class RustWebViewClient: WebViewClient() {
+    override fun shouldInterceptRequest(
+        view: WebView,
+        request: WebResourceRequest
+    ): WebResourceResponse? {
+        return handleRequest(request)
+    }
+
+    companion object {
+        init {
+            System.loadLibrary("app")
+        }
+    }
+
+    private external fun handleRequest(request: WebResourceRequest): WebResourceResponse?
+
+    
+    @android.annotation.SuppressLint("WebViewClientOnReceivedSslError")
+    override fun onReceivedSslError(view: WebView?, handler: SslErrorHandler, error: android.net.http.SslError) {
+        handler.proceed()
+    }
+
+}
